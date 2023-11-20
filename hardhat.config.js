5bf6dae775536a34102c01bb7650522edcc73b12
Dmitry Redkin <tanatonaut@gmail.com>
2023-11-21 00:22:28 +0200

    deploy version

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,48 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+      },
+      {
+        version: "0.8.9",
+      },
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999,
+          },
+        },
+      }
+    ],
+  },
+  networks: {
+    hardhat: {
+    },
+    chapel: {
+      url: process.env.BSC_TESTNET_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    bsc: {
+      url: process.env.BSC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,13 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Router = await hre.ethers.getContractFactory("UniswapV2Router02");
+  const router = await Router.deploy("0xb7f907f7a9ebc822a80bd25e224be42ce0a698a0", "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14");
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await router.deployed();
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `Router deployed to ${router.address}`
   );
 }
 
